ce6833ed0197fb3e51a82c6943b6744dde1fce35
youngseok Kim <youngseokkim@youngseokui-MacBook-Pro.local>
2015-05-10 20:41:41 +0900

    카피 끝

--- UBISEcallGS01/ViewController.h
+++ UBISEcallGS01/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *locationTxt;
 
+- (IBAction)click:(id)sender;
+- (void)retunData:(NSDictionary*)data;
 
 @end
-
